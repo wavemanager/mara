cbae8c9ee239988732b69e5785a6e4bb527f683f
till.mueller <till.mueller@study.hs-duesseldorf.de>
2025-06-07 20:12:20 +0200

    bla bla

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,13 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html'
-		})
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
